211bfa6721bdb0ea0ea64b9639b1ff7e81959662
Shivani Sivakumar <shivanisivakumar0903@gmail.com>
2026-08-27 16:07:26 +0530

    Add resend dependency

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 8000,
+  greetingTimeout: 8000,
+  socketTimeout: 8000,
 });
 
 async function sendEmail(to, subject, text) {
